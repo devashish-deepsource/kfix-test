08cbd947a1e87a391f28d86bc64655b5dbfe99ec
devashish-deepsource <devashish@deepsource.io>
2023-08-25 01:50:42 +0530

    Include new changes

--- Sample.kt
+++ Sample.kt
@@ -1,2 +1,23 @@
-fun haveFun() {
+abstract interface ExFixOptionalAbstractKeyword {
+    abstract val number: Int
+    abstract fun method()
+}
+
+fun exUnnecessaryNotNullOperator() {
+    val num = 5
+    println(num!!.toString())
+}
+
+fun exUnnecessarySafeCall() {
+    val ten = 10
+    println(ten.toString()?.length)
+}
+
+internal class ExNestedClassesVisibility {
+    public class Nested
+}
+
+fun exEqualsNullCall() {
+    val name = "John Doe"
+    println(name.equals(null))
 }
